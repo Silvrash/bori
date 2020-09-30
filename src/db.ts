f732b9497a0d4ee720d1e785b6402af7adfb4858
Benjamin Arko Afrasah <barkoafrasah@gmail.com>
2020-09-30 14:45:28 +0000

    feat(db): cassandra database connectors and query functions

---
+++ src/db.ts
@@ -0,0 +1,47 @@
+import cass from 'cassandra-driver';
+
+// tslint:disable-next-line: no-var-requires
+require('dotenv').config(); // load .env environment vars
+
+/**
+ * get a client instance of the database
+ */
+export const getDatabaseClient = () => {
+	return new cass.Client({
+		contactPoints: process.env.CASSANDRA_CLUSTERS!.split(','),
+		localDataCenter: 'datacenter1',
+		keyspace: 'bori'
+	});
+};
+
+/**
+ * Execute prepared query
+ *
+ * Prepared statements are parsed and prepared on the Cassandra nodes
+ * and are ready for future execution.
+ * Also, when preparing, the driver retrieves information about
+ * the parameter types which allows an accurate mapping
+ * between a JavaScript type and a Cassandra type.
+ * @param  {cass.Client} client - cassandra client instance
+ * @param  {string} preparedQuery - query string
+ * @param  {Array<string|number|boolean>} params? - query parameters
+ */
+const runPreparedQuery = (
+	client: cass.Client,
+	preparedQuery: string,
+	params?: Array<string | number | boolean>
+) => {
+	return client.execute(preparedQuery, params, { prepare: true });
+};
+
+/**
+ * Higher order function which executes a query string
+ * within the scope of the cassandra database
+ * @param  {(type:string,params?:any[])=>string} queryBuilder
+ */
+export const inQuery = (queryBuilder: (type: string, params?: any[]) => string) => {
+	return async (client: cass.Client, type: string, params?: any[]): Promise<any[]> => {
+		const result = await runPreparedQuery(client, queryBuilder(type, params), params);
+		return result.rows;
+	};
+};
